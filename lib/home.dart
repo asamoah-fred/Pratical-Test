bc10d89411f10de5d731dd0d4f42d58968f498cc
asamoah-fred.com <asamoahgodfred42@gmail.com>
2022-04-01 10:05:53 +0100

    added Icon and Text in homescreen

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -18,17 +19,44 @@ class _HomeState extends State<Home> {
         elevation: 0,
         backgroundColor: Colors.blueAccent,
       ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            height: 250,
-            color: Colors.blueAccent,
-            child: Column(
-              children: [
-              ],
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: 400,
+              color: Colors.blueAccent,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Center(
+                    child: Icon(
+                      Icons.school_outlined,
+                      size: 100,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    "TeamTrics",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+            TextFormField(),
+          ],
+        ),
       ),
     );
   }
