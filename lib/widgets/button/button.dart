a3d2006ecb2385f529ecea6d49530c187354030f
asamoah-fred.com <asamoahgodfred42@gmail.com>
2022-04-02 15:42:21 +0100

    added ButtonData to Button file

--- lib/widgets/button/button.dart
+++ lib/widgets/button/button.dart
@@ -1,23 +1,25 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:practical_test/widgets/button/button_data.dart';
 
 class Button extends StatelessWidget {
-  const Button({ Key? key }) : super(key: key);
-
+  const Button({required this.button, Key? key}) : super(key: key);
+  final ButtonData button;
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 250, 
+      width: 250,
       height: 50,
       decoration: BoxDecoration(
         color: Colors.blueAccent,
       ),
-      child: Text('LOGIN',
-      style: TextStyle(
-        fontSize: 18,
-        color: Colors.white,
-      ),
+      child: Text(
+        button.text,
+        style: TextStyle(
+          fontSize: 18,
+          color: Colors.white,
+        ),
       ),
     );
   }
